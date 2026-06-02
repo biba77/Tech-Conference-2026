eb51b6529df021e5b6d905e4cb560fd09b38a4c4
Habiba Tarek Abdalla Mohamed Hassouna <habibahassouna74@gmail.com>
2026-06-02 15:34:17 +0300

    added Dockerfile and port config for deployment

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be44de006959561a329a04827c7e9a615527687")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
